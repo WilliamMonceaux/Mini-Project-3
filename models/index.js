d4c7aa63f75ce0a0334416d0ad86f33a7875cc7c
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-27 08:24:44 -0600

    imported Product model and syncing it along with User model

--- models/index.js
+++ models/index.js
@@ -1,12 +1,15 @@
 'use strict'
 const User = require('./user')
+const Product = require('./product');
 
 async function init() {
     await User.sync();
+    await Product.sync();
 }
 
 init();
 
 module.exports = {
-    User
+    User,
+    Product
 }
