2aa8f906b5ee0ef33588b8f553d6836debc89d4f
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-28 16:59:47 -0600

    moved app.use(express.json()) to near top of file that fixed a bug where nothing would load errors on Thunderclient

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 
 const app = express();
+app.use(express.json());
 
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes );
 
-app.use(express.json());
 
 module.exports = app;
