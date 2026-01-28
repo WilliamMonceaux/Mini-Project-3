f5a66e34f87c273a334f3c62108201a5b785ee1b
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-28 07:52:45 -0600

    imported connectMysql function from dbConnect; created promise that will initialize the server from the connectmysql function

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const app = require("./app");
 require("dotenv").config();
-const dbConnect = require("./dbConnect");
+const { connectMysql } = require("./dbConnect");
+
+let userRoutes = require('./routes/userRoutes');
+app.use('/api/users', userRoutes);
 
 const startServer = async () => {
-  await dbConnect();
+    try {
+        await connectMysql();
 
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    } catch (err) {
+        console.error("Database connection error", err);
+        process.exit(1);
+    }
 };
+
+startServer();
