49c30fe2a670c58628d59a74555d535701d68eb5
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-28 08:25:03 -0600

    added functions 'updateUser' and 'deleteUser' to allow a user to update their credentials or delete them all together

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,27 +1,56 @@
-'use strict';
-const Models = require('../models');
+"use strict";
+const Models = require("../models");
 
 const getUsers = (res) => {
-    Models.User.findAll({}).then(data => {
-        res.send({ result: 200, data: data});
-    }).catch(err => {
-        console.log(err);
-        res.send({ result: 500, error: err.message });
+  Models.User.findAll({})
+    .then((data) => {
+      res.send({ result: 200, data: data });
     })
-}
+    .catch((err) => {
+      console.log(err);
+      res.send({ result: 500, error: err.message });
+    });
+};
 
 const createUser = (data, res) => {
-    Models.User.create(data)
-    .then(data => {
-        res.send({ result: 200, data: data});
+  Models.User.create(data)
+    .then((data) => {
+      res.send({ result: 200, data: data });
     })
-    .catch(err => {
-        console.log(err);
-        res.send({ result: 500, error: err.message});
+    .catch((err) => {
+      console.log(err);
+      res.send({ result: 500, error: err.message });
+    });
+};
+
+const updateUser = (req, res) => {
+  Models.User.update(req.body, {
+    where: { id: req.params.id },
+    returning: true,
+  })
+    .then((data) => {
+      res.send({ result: 200, data: data });
     })
-}
+    .catch((error) => {
+      console.error(error);
+      res.send({ result: 500, error: error.message });
+    });
+};
+
+const deleteUser = (req, res) => {
+  Models.User.destroy({ where: { id: req.params.id } })
+    .then((data) => {
+      res.send({ result: 200, data: data });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send({ result: 500, error: error.message });
+    });
+};
 
 module.exports = {
-    getUsers, 
-    createUser
+  getUsers,
+  createUser,
+  updateUser,
+  deleteUser
 };
