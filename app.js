425950afde85de3f5071b23b8c85b93fe3bae2cc
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-28 08:21:45 -0600

    imported userRoutes; changed productRoute from '/api/shop' to '/api/products'

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 
 const app = express();
 
-app.use(express.json());
+app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes );
 
-app.use('/api/shop', productRoutes );
+app.use(express.json());
 
 module.exports = app;
