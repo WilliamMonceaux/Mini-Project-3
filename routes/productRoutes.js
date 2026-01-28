337fa26f053de49b6d9837f1ea1a0f6eaf206ae7
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-28 08:26:27 -0600

    changed route directory from '/products' to '/' for ProductList

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { productList } = require('../controllers/productController');
+const { productList } = require("../controllers/productController");
 
-router.get('/products', productList )
+router.get("/", productList);
 
 module.exports = router;
