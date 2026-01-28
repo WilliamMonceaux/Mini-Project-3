ec3d21bdbbad881465da9c24217115f1b84d0e44
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-28 07:51:38 -0600

    renammed productRoute to productRoutes

--- routes/productRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { productList } = require('../controllers/productController');
-
-router.get('/products', productList )
-
-module.exports = router;
