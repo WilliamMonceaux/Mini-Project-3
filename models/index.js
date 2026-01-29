b1702600c67195365a155fa64cfbd69636ef3c6d
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-29 07:49:41 -0600

    imported and synced Order model

--- models/index.js
+++ models/index.js
@@ -1,15 +1,21 @@
-'use strict'
-const User = require('./user')
-const Product = require('./product');
+"use strict";
+const User = require("./user");
+const Product = require("./product");
+const Order = require("./order");
+
+User.hasMany(Order);
+Order.belongsTo(User);
 
 async function init() {
-    await User.sync();
-    await Product.sync();
+  await User.sync();
+  await Product.sync();
+  await Order.sync();
 }
 
 init();
 
 module.exports = {
-    User,
-    Product
-}
+  User,
+  Product,
+  Order,
+};
