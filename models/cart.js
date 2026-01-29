8c7427a2b9f08720acd0b5ddb8079d27264acc5f
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-29 17:10:09 -0600

    implemented Cart model with its respected fields

---
+++ models/cart.js
@@ -0,0 +1,44 @@
+const { DataTypes, Model } = require("sequelize");
+let { sequelize } = require("../config/dbConnect");
+
+class Cart extends Model {}
+
+Cart.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: "users", key: "id" },
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+      validate: {
+        min: 1,
+      },
+    },
+    unitPrice: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "carts",
+    timestamps: true,
+    freezeTableName: true,
+  },
+);
+
+module.exports = Cart;
