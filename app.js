e06e4cd9fb7b8f24d397c030371909c38b90c63a
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-29 07:47:49 -0600

    refactored with prettier

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const userRoutes = require('./routes/userRoutes');
-const productRoutes = require('./routes/productRoutes');
+const express = require("express");
+const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
+const orderRoutes = require("./routes/orderRoutes");
 
 const app = express();
 app.use(express.json());
 
-app.use('/api/users', userRoutes);
-app.use('/api/products', productRoutes );
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes);
 
 
 module.exports = app;
