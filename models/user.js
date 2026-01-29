f57fbaa5d28e77dd1f2e74c030d6e586e4a6ef8d
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-29 07:52:40 -0600

    updated sequelize import pathing with config/dbConnect

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const { DataTypes, Model } = require("sequelize");
-let { sequelize } = require("../dbConnect");
+let { sequelize } = require("../config/dbConnect");
 
 class User extends Model {}
 
