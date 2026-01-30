53fc9f533010ec906b9f40dde98fa48b80c27911
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-30 17:07:41 -0600

    added CRUD operations for products

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,21 +1,71 @@
-const BASE_URL = 'http://fakestoreapi.com/products';
+"use strict";
+const Models = require("../models");
 
-const productList = async (req, res) => {
-    try {
-        const response = await fetch(BASE_URL);
+const BASE_URL = "http://fakestoreapi.com/products";
 
-        if (!response.ok) {
-            return res.status(response.status).json({error: "Couldn't fetch product data"});
-        }
+const getProducts = async (req, res) => {
+  try {
+    const apiPromise = fetch(BASE_URL);
+    const dbPromise = Models.Product.findAll({});
 
-        const productData = await response.json();
+    const [apiResponse, dbProducts] = await Promise.all([
+      apiPromise,
+      dbPromise,
+    ]);
 
-        res.status(200).json(productData);
-        
-    } catch (error) {
-        console.error(`Error! Couldn't fetch fetch product data. Error: ${error.message}`);
-        res.status(500).json({error: 'Failed to fetch products'});
+    if (!apiResponse.ok) {
+      return res
+        .status(apiResponse.status)
+        .json({ error: "Couldn't fetch product data" });
     }
-}
 
-module.exports = { productList }
+    const apiProducts = await apiResponse.json();
+
+    const combinedProducts = [...dbProducts, ...apiProducts];
+
+    res.status(200).json(combinedProducts);
+  } catch (error) {
+    console.error(
+      `Error! Couldn't fetch fetch product data. Error: ${error.message}`,
+    );
+    res.status(500).json({ error: "Failed to fetch products" });
+  }
+};
+
+const createProduct = async (req, res) => {
+  Models.Product.create(req.body)
+    .then((data) => {
+      res.send({ result: 200, data: data });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.send({ result: 500, error: err.message });
+    });
+};
+
+const updateProduct = (req, res) => {
+  Models.Product.update(req.body, {
+    where: { id: req.params.id },
+    returning: true,
+  })
+    .then((data) => {
+      res.send({ result: 200, data: data });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.send({ result: 500, error: err.message });
+    });
+};
+
+const deleteProduct = (req, res) => {
+  Models.Product.destroy({ where: { id: req.params.id } })
+    .then((data) => {
+      res.send({ result: 200, data: data });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.send({ result: 500, error: err.message });
+    });
+};
+
+module.exports = { getProducts, createProduct, updateProduct, deleteProduct };
