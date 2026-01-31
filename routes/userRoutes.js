6a8b46110651771208ff1cc006eb25b965e0f768
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-31 09:25:50 -0600

    refactored with prettier

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getUsers, createUser, updateUser, deleteUser} = require('../controllers/userController');
+const {
+  getUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/userController");
 
-router.get('/', getUsers);
+router.get("/", getUsers);
 
-router.post('/', createUser);
+router.post("/", createUser);
 
-router.put('/:id', updateUser);
+router.put("/:id", updateUser);
 
-router.delete('/:id', deleteUser);
+router.delete("/:id", deleteUser);
 
 module.exports = router;
