bc95a9e032bc500ac672f8e141f5b9b58ed14770
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-31 10:49:29 -0600

    fixed the routes directory that was mistakenly mistyped from 'orderRoutes' to 'cartRoutes'

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 const orderRoutes = require("./routes/orderRoutes");
+const cartRoutes = require("./routes/cartRoutes");
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,6 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
-
+app.use("/api/carts", cartRoutes);
 
 module.exports = app;
