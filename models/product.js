cf7db5809b37bcfd30a6151cfe7476263fcde70b
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-02-04 08:30:20 -0600

    implemented rating property that tracks the rating of a product and how much of the product is available

--- models/product.js
+++ models/product.js
@@ -11,17 +11,32 @@ Product.init(
       autoIncrement: true,
       primaryKey: true,
     },
+    externalId: {
+      type: DataTypes.INTEGER,
+      allowNull: true, 
+      unique: true,    
+    },
     title: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        notEmpty: { msg: "Title cannot be empty" },
+      },
     },
     price: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
+      validate: {
+        isDecimal: true,
+        min: { args: [0], msg: "Price cannot be negative" },
+      },
     },
     description: {
       type: DataTypes.STRING(1000),
       allowNull: false,
+      validate: {
+        notEmpty: { msg: "Description is required" },
+      },
     },
     category: {
       type: DataTypes.STRING,
@@ -30,6 +45,14 @@ Product.init(
     image: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        isUrl: { msg: "Image must be a valid URL link" },
+      },
+    },
+    rating: {
+      type: DataTypes.JSON,
+      allowNull: false,
+      defaultValue: { rate: 0, count: 0 },
     },
   },
   {
